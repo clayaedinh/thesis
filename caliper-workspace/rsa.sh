c30c40c8292f399664b777959596c3b159217b2e
Aedin Clay <clayaedinh@gmail.com>
2023-04-11 14:42:49 +0800

    tps measurement

--- caliper-workspace/rsa.sh
+++ caliper-workspace/rsa.sh
@@ -1,4 +1,21 @@
-npx caliper launch manager \
-    --caliper-workspace . \
-    --caliper-benchconfig benchmarks/rsa/config.yaml \
-    --caliper-networkconfig networks/thesis-network.yaml
+maxTps() {
+    npx caliper launch manager \
+        --caliper-workspace . \
+        --caliper-benchconfig benchmarks/rsa/config-max-tps.yaml \
+        --caliper-networkconfig networks/thesis-network.yaml
+}
+
+fixedTps() {
+    npx caliper launch manager \
+        --caliper-workspace . \
+        --caliper-benchconfig benchmarks/rsa/config.yaml \
+        --caliper-networkconfig networks/thesis-network.yaml
+}
+
+if [ "$1" = "max" ]; then
+    maxTps
+elif [ "$1" = "fixed" ]; then
+    fixedTps
+else
+    echo "arguments: max | fixed"
+fi
